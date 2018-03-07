9574c002f009e856c91f111bcf7ef6e74e32e8e2
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-03-06 19:23:05 -0800

    fizz buzz song loop

--- src/modulo/FizzBuzz.java
+++ src/modulo/FizzBuzz.java
@@ -26,12 +26,29 @@ package modulo;
 **/
 
 public class FizzBuzz {
+	
 
 public static void main(String[] args) {
 	
-	for()for (int i = 0; i < 19; i++) {
-		
-		
+	for (int i = 1; i <= 20; i++) {
+		if(i%3==0 ) {
+			System.out.println("fizz");
+		}
+		else {
+			System.out.println(i);
+		}
+		if(i%5==0) {
+			System.out.println("buzz");
+		}
+		else {
+			System.out.println(i);
+		}
+		if(i%3==0||i%5==0) {
+			System.out.println("fizz buzz");
+		}
+		else {
+			System.out.println(i);
+		}
 	}
 }
 
---
+++ src/song.java
@@ -0,0 +1,47 @@
+
+public class song {
+	
+
+public static void main(String[] args) {
+
+	
+	for (int i = 1; i < 9; i++) {
+		 System.out.println(i+ " potato");
+		
+	
+	if(i==4) {
+		System.out.println(i);
+	}
+	else if(i==8) {
+		System.out.println("more");
+	}
+	
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
